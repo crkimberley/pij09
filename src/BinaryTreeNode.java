87545b794cf76edd4c7c6a50b9de9155faa2e652
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-09-28 19:05:13 +0100

    BinaryTreeNode - now nesting static classes

--- src/BinaryTreeNode.java
+++ src/BinaryTreeNode.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import static java.lang.Character.isDigit;
+
 /**
  * @author crkimberley on 28/09/2016.
  */
@@ -17,8 +20,121 @@ public class BinaryTreeNode {
         this.right = right;
     }
 
+    public BinaryTreeNode() {
+        value = null;
+        left = null;
+        right = null;
+    }
+
     public String toString() {
         return "[" + value + " L" + (left != null ? left : "[]")
                 + " R" + (right != null ? right : "[]") + "]";
+    }
+
+    public static BinaryTreeNode mathExpressionToTree(String expression) {
+        return new ShuntingYardParser().expressionToTree(expression);
+    }
+
+    private static class ShuntingYardParser {
+
+        private Collection<Operator> operators;
+        private Map<Character, Operator> operatorMap;
+
+        private static void addNode(Stack<BinaryTreeNode> stack, char operator) {
+            BinaryTreeNode right = stack.pop();
+            BinaryTreeNode left = stack.pop();
+            stack.push(new BinaryTreeNode(operator, left, right));
+        }
+
+        public ShuntingYardParser() {
+            // Define mathematical operators
+            operators = new ArrayList<Operator>();
+            operators.add(new Operator('*', 2));
+            operators.add(new Operator('/', 2));
+            operators.add(new Operator('+', 1));
+            operators.add(new Operator('-', 1));
+            operatorMap = new HashMap<Character, Operator>();
+            for (Operator operator : operators) {
+                operatorMap.put(operator.getSymbol(), operator);
+            }
+        }
+
+        public BinaryTreeNode expressionToTree(String input) {
+            Stack<Character> operatorStack = new Stack<Character>();
+            Stack<BinaryTreeNode> operandStack = new Stack<BinaryTreeNode>();
+            char[] chars = input.toCharArray();
+            main:
+            for (int i=0; i<chars.length; i++) {
+                char c = chars[i];
+                char popped;
+                switch (c) {
+                    case ' ':
+                        break;
+                    case '(':
+                        operatorStack.push('(');
+                        break;
+                    case ')':
+                        while (!operatorStack.isEmpty()) {
+                            popped = operatorStack.pop();
+                            if ('(' == popped) {
+                                continue main;
+                            } else {
+                                addNode(operandStack, popped);
+                            }
+                        }
+                    default:
+                        if (operatorMap.containsKey(c)) {
+                            Operator operator1 = operatorMap.get(c);
+                            Operator operator2;
+                            while (!operatorStack.isEmpty() && null != (operator2 =
+                                    operatorMap.get(operatorStack.peek()))) {
+                                if (operator1.comparePrecedence(operator2) <= 0) {
+                                    operatorStack.pop();
+                                    addNode(operandStack, operator2.getSymbol());
+                                } else {
+                                    break;
+                                }
+                            }
+                            operatorStack.push(c);
+                        } else {
+                            StringBuilder numberBuilder = new StringBuilder(Character.toString(c));
+                            while ((i+1) < chars.length && isDigit(chars[i+1])) {
+                                numberBuilder.append(chars[i+1]);
+                                i++;
+                            }
+                            operandStack.push(new BinaryTreeNode(numberBuilder.toString(), null, null));
+                        }
+                        break;
+                }
+            }
+            while (!operatorStack.isEmpty()) {
+                addNode(operandStack, operatorStack.pop());
+            }
+            return operandStack.pop();
+        }
+
+        private static class Operator {
+
+            private char symbol;
+            private int precedence;
+
+            public Operator(char symbol, int precedence) {
+                this.symbol = symbol;
+                this.precedence = precedence;
+            }
+
+            public int comparePrecedence(Operator operator) {
+                return precedence > operator.precedence ? 1 : operator.precedence == precedence ? 0 : -1;
+            }
+
+            public char getSymbol() {
+                return symbol;
+            }
+
+            @Override
+            public String toString() {
+                return Character.toString(symbol);
+            }
+        }
     }
 }
--- src/BinaryTreeNodeTest.java
+++ src/BinaryTreeNodeTest.java
@@ -1,17 +1,16 @@
 /**
  * @author crkimberley on 28/09/2016.
  */
-public class ShuntingYardParserTest {
+public class BinaryTreeNodeTest {
 
     public static void main(String[] args) {
-        new ShuntingYardParserTest().launch();
+        new BinaryTreeNodeTest().launch();
     }
 
     private void launch() {
-        ShuntingYardParser parser = new ShuntingYardParser();
         String[] expressions = {"(3 + 3) * (8 - 11 * 2)", "123 + 456", "2/3*4"};
         for (String expression : expressions) {
-            BinaryTreeNode tree = parser.expressionToTree(expression);
+            BinaryTreeNode tree = BinaryTreeNode.mathExpressionToTree(expression);
             System.out.println("Mathematical expression: " + expression + "\nAbstract Syntax Tree: " + tree);
         }
     }
